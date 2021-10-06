4c55d67ee48b22f666f92dd7f7a579952e58a642
shasib <saqib_hasib@sfu.ca>
2021-10-06 09:26:37 -0700

    Added dynamic rendering functionality for 2 types of users

--- client/src/components/Sidenav.tsx
+++ client/src/components/Sidenav.tsx
@@ -1,71 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../assets/logo.svg';
-import { CgLogOut } from 'react-icons/cg';
-import { FiBarChart2, FiBriefcase } from 'react-icons/fi';
-import * as BsIcons from 'react-icons/bs';
 import '../styles/Sidenav.css';
+import { volunteerItems1, volunteerItems2, adminItems1, adminItems2 } from './SidenavItems';
+import { Link } from 'react-router-dom';
+
+
+const userTypes = [ "admin", "volunteer" ];
 
 export default function Sidenav() {
+    // cited from: https://stackoverflow.com/questions/44480053/how-to-detect-if-screen-size-has-changed-to-mobile-in-react
+    const [isMobile, setIsMobile] = useState(false);
+    const userType = userTypes[1];
+
+    const manageResize = () => {
+        if(window.innerWidth < 768) {
+            setIsMobile(true);
+        } else {
+            setIsMobile(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", manageResize)
+        return () => {
+            window.removeEventListener("resize", manageResize)
+        }
+    }, [isMobile]);
+
+    let menus1;    
+    let menus2;    
+    
+    const renderMenus = () => {
+        if(userType === userTypes[0]) {
+            menus1 = adminItems1.map((item, index) => {
+                return(
+                    <li className="nav-item btn fs-5 border-3" key={index}>
+                        <Link to={item.to} className="nav-link py-0 btn-font-color-secondary" aria-current="page">
+                            {item.icon}
+                        </Link>
+                    </li>
+                );
+            });
+            menus2 = adminItems2.map((item, index) => {
+                return(
+                    <li className="nav-item btn fs-5 border-3" key={index}>
+                        <Link to={item.to} className="nav-link py-0 btn-font-color-secondary" aria-current="page">
+                            {item.icon}
+                        </Link>
+                    </li>
+                );
+            });
+        } else {
+            menus1 = volunteerItems1.map((item, index) => {
+                return(
+                    <li className="nav-item btn fs-5 border-3" key={index}>
+                        <Link to={item.to} className="nav-link py-0 btn-font-color-secondary" aria-current="page">
+                            {item.icon}
+                        </Link>
+                    </li>
+                );
+            });
+            menus2 = volunteerItems2.map((item, index) => {
+                return(
+                    <li className="nav-item btn fs-5 border-3" key={index}>
+                        <Link to={item.to} className="nav-link py-0 btn-font-color-secondary" aria-current="page">
+                            {item.icon}
+                        </Link>
+                    </li>
+                );
+            });
+        }
+    }
+
+    renderMenus();
+
     return (
         <>        
             <div className="navbar l-navbar"> 
                 <div className="container nav-container">   
                     <div className="mt-2 nav-top-container">                
-                        <a href="#" className="navbar-brand">
+                        <Link to="/" className="navbar-brand">
                             <img src={logo} alt="logo" />
-                        </a>
+                        </Link>
                         <div className="mt-3">
                             <ul className='navbar-nav flex-column'>
-                                <li className="nav-item btn fs-5 border-3">
-                                    <a className="nav-link py-0 btn-font-color-secondary" aria-current="page" href="#">
-                                        <BsIcons.BsSearch className="nav-icon"/>
-                                    </a>
-                                </li>
-                                <li className="nav-item btn fs-5 border-3">
-                                    <a className="nav-link py-0 btn-font-color-secondary" aria-current="page" href="#">
-                                        <BsIcons.BsHouseDoor className="nav-icon"/>
-                                    </a>
-                                </li>
-                                <li className="nav-item btn fs-5 border-3">
-                                    <a className="nav-link py-0 btn-font-color-secondary" aria-current="page" href="#">
-                                        <BsIcons.BsCalendar2Check className="nav-icon"/>
-                                    </a>
-                                </li>
-                                <li className="nav-item btn fs-5 border-3">
-                                    <a className="nav-link py-0 btn-font-color-secondary" aria-current="page" href="#">
-                                        <BsIcons.BsChatLeft className="nav-icon"/>
-                                    </a>
-                                </li>
-                                <li className="nav-item btn fs-5 border-3">
-                                    <a className="nav-link py-0 btn-font-color-secondary" aria-current="page" href="#">
-                                        <FiBriefcase className="nav-icon"/>
-                                    </a>
-                                </li>
-                                <li className="nav-item btn fs-5 border-3">
-                                    <a className="nav-link py-0 btn-font-color-secondary" aria-current="page" href="#">
-                                        <FiBarChart2 className="nav-icon"/>
-                                    </a>
-                                </li>
+                                {menus1}
                             </ul>
                         </div>
                     </div>
                     <div className="mb-3">
                         <ul className='navbar-nav flex-column'>
-                            <li className="nav-item btn fs-5 border-3">
-                                <a className="nav-link py-0 btn-font-color-secondary" aria-current="page" href="#">
-                                    <BsIcons.BsBell className="nav-icon"/>
-                                </a>
-                            </li>
-                            <li className="nav-item btn fs-5 border-3">
-                                <a className="nav-link py-0 btn-font-color-secondary" aria-current="page" href="#">
-                                    <BsIcons.BsGear className="nav-icon"/>
-                                </a>
-                            </li>
-                            <li className="nav-item btn fs-5 border-3">
-                                <a className="nav-link py-0 btn-font-color-secondary" aria-current="page" href="#">
-                                    <CgLogOut className="nav-icon"/>
-                                </a>
-                            </li>
+                            {menus2}
                         </ul>
                     </div>                 
                 </div>               
---
+++ client/src/components/SidenavItems.tsx
@@ -0,0 +1,72 @@
+import { CgLogOut } from 'react-icons/cg';
+import { FiBarChart2, FiBriefcase } from 'react-icons/fi';
+import * as BsIcons from 'react-icons/bs';
+
+export const volunteerItems1 = [
+    {
+        to: "/search",
+        icon: <BsIcons.BsSearch className="nav-icon"/>
+    },
+    {
+        to: "/home",
+        icon: <BsIcons.BsHouseDoor className="nav-icon"/>
+    },
+    {
+        to: "/timecard",
+        icon: <BsIcons.BsCalendar2Check className="nav-icon"/>
+    },
+    {
+        to: "/messages",
+        icon: <BsIcons.BsChatLeft className="nav-icon"/>
+    },
+    {
+        to: "/questionnaire",
+        icon: <FiBriefcase className="nav-icon"/>
+    },
+    {
+        to: "/report",
+        icon: <FiBarChart2 className="nav-icon"/>
+    },
+];
+export const volunteerItems2 = [
+    {
+        to: "/notifications",
+        icon: <BsIcons.BsBell className="nav-icon"/>
+    },
+    {
+        to: "/settings",
+        icon: <BsIcons.BsGear className="nav-icon"/>
+    },
+    {
+        to: "/logout",
+        icon: <CgLogOut className="nav-icon"/>
+    },
+];
+export const adminItems1 = [
+    {
+        to: "/search",
+        icon: <BsIcons.BsSearch className="nav-icon"/>
+    },
+    {
+        to: "/home",
+        icon: <BsIcons.BsHouseDoor className="nav-icon"/>
+    },
+    {
+        to: "/messages",
+        icon: <BsIcons.BsChatLeft className="nav-icon"/>
+    },
+    {
+        to: "/notifications",
+        icon: <BsIcons.BsBell className="nav-icon"/>
+    },
+    {
+        to: "/settings",
+        icon: <BsIcons.BsGear className="nav-icon"/>
+    },
+];
+export const adminItems2 = [
+    {
+        to: "/logout",
+        icon: <CgLogOut className="nav-icon"/>
+    },
+];
