11a12bc6a75fcdafdbe3c93b816ef6ce7412fab2
shasib <saqib_hasib@sfu.ca>
2021-10-06 16:11:20 -0700

    Created mobile-friendly UI for Sidenav

--- client/src/components/Sidenav.tsx
+++ client/src/components/Sidenav.tsx
@@ -3,14 +3,25 @@ import logo from '../assets/logo.svg';
 import '../styles/Sidenav.css';
 import { volunteerItems1, volunteerItems2, adminItems1, adminItems2 } from './SidenavItems';
 import { Link } from 'react-router-dom';
+import Hamburger from 'hamburger-react';
 
 
 const userTypes = [ "admin", "volunteer" ];
 
+function getScreenSize () {
+    if(window.innerWidth < 768) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 export default function Sidenav() {
-    // cited from: https://stackoverflow.com/questions/44480053/how-to-detect-if-screen-size-has-changed-to-mobile-in-react
-    const [isMobile, setIsMobile] = useState(false);
     const userType = userTypes[1];
+
+    // cited from: https://stackoverflow.com/questions/44480053/how-to-detect-if-screen-size-has-changed-to-mobile-in-react
+    const [isMobile, setIsMobile] = useState(getScreenSize);
+    const [hamburgerOpen, setHamburgerOpen] = useState(false);
 
     const manageResize = () => {
         if(window.innerWidth < 768) {
@@ -20,16 +31,42 @@ export default function Sidenav() {
         }
     }
 
+    let menus1;    
+    let menus2;
+
     useEffect(() => {
-        window.addEventListener("resize", manageResize)
+        window.addEventListener("resize", manageResize);
         return () => {
             window.removeEventListener("resize", manageResize)
         }
     }, [isMobile]);
 
-    let menus1;    
-    let menus2;    
-    
+
+    // function handleHamburger (){
+    //     setHamburgerOpen(!hamburgerOpen);
+    // }
+
+    function HeaderRender () {
+        return(
+            <div className={`header fixed-top bg-transparent topnav ${hamburgerOpen ? 'open' : ''} ${isMobile ? '': 'collapse'}`}>
+                <ul className="navbar-nav flex-row bg-transparent">
+                    <li className="nav-item p-0 ">
+                        <a className="nav-link py-0" aria-current="page">
+                            <Hamburger
+                                toggled={hamburgerOpen} 
+                                toggle={setHamburgerOpen}
+                                duration={0.6}
+                                size={26}
+                                rounded
+                                color="black"
+                            />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+
     const renderMenus = () => {
         if(userType === userTypes[0]) {
             menus1 = adminItems1.map((item, index) => {
@@ -76,7 +113,8 @@ export default function Sidenav() {
 
     return (
         <>        
-            <div className="navbar l-navbar"> 
+            <HeaderRender/>
+            <div className={`${hamburgerOpen ? 'open' : ''} navbar l-navbar shadow`}> 
                 <div className="container nav-container">   
                     <div className="mt-2 nav-top-container">                
                         <Link to="/" className="navbar-brand">
