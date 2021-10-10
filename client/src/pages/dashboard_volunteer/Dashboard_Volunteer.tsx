3be38244dee348f4010056eb57dfffc55a4ed53e
eha45 <eha45@sfu.ca>
2021-10-10 13:39:28 -0700

    initial creation of volunteer's dashboard

---
+++ client/src/pages/dashboard_volunteer/Dashboard_Volunteer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import container from 'react-bootstrap'
+
+function Dashboard_Volunteer() {
+    return (
+        <div >
+            
+        </div>
+    )
+}
+
+export default Dashboard_Volunteer;
