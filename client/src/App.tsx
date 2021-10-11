9c8553ff474d5fa5108fbd4a9183856f5dbaecb5
Shan Qing <sqing@sfu.ca>
2021-10-10 20:36:59 -0700

    finished the basic design of the messages page, but still need real messages to fill in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import store from "./store";
 import Login from './pages/login/Login';
 import Sidenav from './components/Sidenav';
 // import Dashboard from './pages/dashboard/Dashboard';
-// import Messages from './pages/messages/Messages';
+ import Messages from './pages/messages/Messages';
 // import Questionnaire from './pages/questionnaire/Questionnaire';
 import Profile from './pages/profile/Profile';
 
@@ -20,7 +20,7 @@ function App() {
           <Route exact path="/login" component={Login}/>
           <Route exact path="/profile" component={Profile}/>
           {/* <Route path="/dashboard" component={Dashboard}/> */}
-          {/* <Route path="/messages" component={Messages}/> */}
+          <Route path="/messages" component={Messages}/>
           {/* <Route path="/questionnaire" component={Questionnaire}/> */}
           {/* <Route path="/settings" component={Settings}/> */}
         </Switch>
