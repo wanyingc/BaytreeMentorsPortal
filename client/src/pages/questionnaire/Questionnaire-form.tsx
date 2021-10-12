5a1629356e3055685888e42f7fab2cd2fa048b57
Josh <jjm19@sfu.ca>
2021-10-11 23:56:08 -0700

    Removed commented code

--- client/src/pages/questionnaire/Questionnaire-form.tsx
+++ client/src/pages/questionnaire/Questionnaire-form.tsx
@@ -25,9 +25,6 @@ const QuestionnaireForm = () => {
             <Form.Control type="text" placeholder="Ex: Jane Smith" />
           </Col>
       </Row>
-      {/* <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
-      </Form.Text> */}
     </Form.Group>
     
     <h5>Practical Support</ h5>
@@ -481,51 +478,3 @@ const QuestionnaireForm = () => {
 export default reduxForm({
     form: 'monthly-questionnaire-form',
 })(QuestionnaireForm);
-
-
-// const [checked, setChecked] = useState(false);
-//const [radioValue, setRadioValue] = useState('');
-
-// const radios = [
-//     { name: '1', value: '1' },
-//     { name: '2', value: '2' },
-//     { name: '3', value: '3' },
-//     { name: '4', value: '4' },
-//     { name: '5', value: '5' },
-//   ];
-
-/* <ToggleButtonGroup type="radio" name="radioO" >
-    <ToggleButton id="tbg-radio-1" value={1} variant='outline-primary' className="toggle-button" >
-       1 
-    </ToggleButton>
-    <ToggleButton id="tbg-radio-2" value={2} variant='outline-primary' className="toggle-button">
-      2
-    </ToggleButton>
-    <ToggleButton id="tbg-radio-3" value={3} variant='outline-primary'>
-      3
-    </ToggleButton>
-    <ToggleButton id="tbg-radio-3" value={4} variant='outline-primary'>
-      4
-    </ToggleButton>
-    <ToggleButton id="tbg-radio-3" value={5} variant='outline-primary'>
-      5
-    </ToggleButton>
-  </ToggleButtonGroup>
-  <br /> */
-
-
-//   <ToggleButtonGroup type="radio" name="radio" >
-//   {radios.map((radio, idx) => (
-//       <ToggleButton
-//           className="toggle-button"
-//           key={idx}
-//           id={`radio-${idx}`}
-//           variant={radioValue === radio.value ? (+radio.value < 3 ? 'danger':'primary') : 'outline-primary'}
-//           value={radio.value}
-//           checked={radioValue === radio.value}
-//           onChange={(e) => setRadioValue(e.currentTarget.value)}
-//           >
-//           {radio.name}
-//       </ToggleButton>
-//   ))}
-// </ToggleButtonGroup>
