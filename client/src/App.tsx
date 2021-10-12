2227eadfd4013fece9da6cdcdf763def54480275
Shan Qing <sqing@sfu.ca>
2021-10-12 01:38:56 -0700

    finished the basic design of report page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,8 @@ import store from "./store";
 import Login from './pages/login/Login';
 import Sidenav from './components/Sidenav';
 // import Dashboard from './pages/dashboard/Dashboard';
- import Messages from './pages/messages/Messages';
+import Messages from './pages/messages/Messages';
+import Report from './pages/report/Report';
 // import Questionnaire from './pages/questionnaire/Questionnaire';
 import Profile from './pages/profile/Profile';
 
@@ -21,6 +22,7 @@ function App() {
           <Route exact path="/profile" component={Profile}/>
           {/* <Route path="/dashboard" component={Dashboard}/> */}
           <Route path="/messages" component={Messages}/>
+          <Route path="/report" component={Report}/>
           {/* <Route path="/questionnaire" component={Questionnaire}/> */}
           {/* <Route path="/settings" component={Settings}/> */}
         </Switch>
