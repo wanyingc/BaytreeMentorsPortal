7aff6e4da03f3aadd5da5a7da88fb4cad14f2d31
Dhairya Kalra <dkalra@sfu.ca>
2021-10-12 11:55:16 -0700

    Connected user login page to database

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import logo from '../../assets/logo192.png';
+import React, { useState } from 'react';
+import logo from '../../assets/logo512.png';
 import './Login.css';
-
+import Container from "react-bootstrap/Container";
+const Axios=require('axios')
 function Login() {
+
+  const [phoneNumber,setLogin]=useState("")
+  const [password,setPass]=useState("")
+
+  const addLogin= ()=>{
+    Axios.post("http://localhost:3001/login",{
+    phoneNumber:phoneNumber,
+    password:password
+  });
+  }
   return (
     <div id="bigBox">
         <div>
@@ -10,15 +21,25 @@ function Login() {
         </div>
         <div className="inputText">
           <form className="inputTitle" id="inputTitle">Phone Number</form>
-          <input type="text" id="inputBox"/>
+          <input type="text"
+           id="inputBox" 
+           onChange={(event)=>{
+             setLogin(event.target.value);
+           }}
+            />
         </div>
 
         <div className="inputText">
           <form className="inputTitle" id="inputTitle">Password</form>
-          <input type="password" id="inputBox"/>
+          <input type="password" id="inputBox"
+           onChange={(event)=>{
+           setPass(event.target.value);
+           }}
+           />
         </div>
-        <div>
-          <button className="login" id="login">log in</button>
+        <div className="submitButton">
+          <button className="btn btn-primary" id="login" 
+          onClick={(addLogin)}>Submit</button>
         </div>
         <div>
           <a href="url" id="signUp">Not a user? Sign up</a>
