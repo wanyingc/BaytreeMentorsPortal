4490674cecb4648534035d1931882e3a8f76b39f
Dhairya Kalra <dkalra@sfu.ca>
2021-10-12 12:17:41 -0700

    Merged changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,9 +6,12 @@ import store from "./store";
 import Login from './pages/login/Login';
 import Sidenav from './components/Sidenav';
 // import Dashboard from './pages/dashboard/Dashboard';
-// import Messages from './pages/messages/Messages';
+import Messages from './pages/messages/Messages';
+import Report from './pages/report/Report';
 // import Questionnaire from './pages/questionnaire/Questionnaire';
+import QuestionnaireForm from './pages/questionnaire/Questionnaire-form';
 import Profile from './pages/profile/Profile';
+import TimeCard from './pages/timecard/TimeCard';
 
 function App() {
   return (
@@ -19,8 +22,11 @@ function App() {
           <Route exact path="/" component={Login}/>
           <Route exact path="/login" component={Login}/>
           <Route exact path="/profile" component={Profile}/>
+          <Route exact path="/timecard" component={TimeCard}/>
+
           {/* <Route path="/dashboard" component={Dashboard}/> */}
-          {/* <Route path="/messages" component={Messages}/> */}
+          <Route path="/messages" component={Messages}/>
+          <Route path="/report" component={Report}/>
           {/* <Route path="/questionnaire" component={Questionnaire}/> */}
           {/* <Route path="/settings" component={Settings}/> */}
         </Switch>
