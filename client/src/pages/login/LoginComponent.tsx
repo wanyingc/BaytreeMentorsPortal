a4dbe37f3e8f9455a4c129fd81cfa45492f0d7c3
shasib <saqib_hasib@sfu.ca>
2021-10-15 18:11:29 -0700

    Quick fix: Change couple of things customer mentioned

--- client/src/pages/login/LoginComponent.tsx
+++ client/src/pages/login/LoginComponent.tsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import logo from '../../assets/logo.svg';
 import './Login.css';
 import { Link } from 'react-router-dom';
-
-const Axios=require('axios')
+import Axios from 'axios';
 
 const LoginComponent = () => {
 
@@ -21,45 +20,44 @@ const LoginComponent = () => {
     <div className="container ">
       <div className="row border-0 d-flex align-items-center ">
         <div className="col mx-auto ">            
-            <img src={logo}  alt="logo" width="auto" height="175" className="image mt-3"/>
-            <h4 className="text-center pt-3">Sign in</h4>
-            <form >
-                <div className="form-group mb-3">
-                  <label>Phone number or Email</label>
-                  <input 
-                    type="text" 
-                    className="form-control"
-                    onChange={(event)=>{
+          <img src={logo}  alt="logo" width="auto" height="175" className="image mt-3"/>
+          <h4 className="text-center pt-3">Sign in</h4>
+          <form >
+            <div className="form-group mb-3">
+              <label>Phone number or Email</label>
+              <input 
+                  type="email" 
+                  className="form-control"
+                  onChange={(event)=>{
                     setLogin(event.target.value);
                   }}
-                    placeholder="Enter phone number or email"
-                    />
-                </div>
-                <div className="form-group mb-3">
-                  <label>Password</label>
-                  <input 
-                    className="form-control"
-                    placeholder="Enter password"
-                    type="password"
-                    onChange={(event)=>{
-                      setPass(event.target.value);
-                    }}
-                  />
-                </div>
-                <Link to="/dashboard">
-                  <button
-                    type="submit"
-                    className="btn btn-primary btn-block" id="login" 
-                    onClick={(addLogin)}
-                  >
-                      Submit
-                  </button>
-                </Link>                  
-            </form>
-          
+                placeholder="Enter email"
+              />
+              </div>
+              <div className="form-group mb-3">
+                <label>Password</label>
+                <input 
+                  className="form-control"
+                  placeholder="Enter password"
+                  type="password"
+                  onChange={(event)=>{
+                    setPass(event.target.value);
+                  }}
+                />
+              </div>
+              <Link to="/dashboard">
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-block" id="login" 
+                  onClick={(addLogin)}
+                >
+                  Sign in
+                </button>
+              </Link>                  
+          </form>          
           <div className="">
             <p className="text-center">
-              <a href="/signup" id="signUp">Not a user? Sign up</a>
+              <a href="/signup" id="signUp">Forgot password</a>
             </p>
           </div>
         </div>
