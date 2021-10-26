5b27aee5628e8e2c7a6033dc0a884cf7f0c1d9df
cagla istanbulluoglu <cagla.istanbulluoglu@gmail.com>
2021-10-25 18:22:38 -0700

    register-API

--- server/src/api/v1/models/user.model.ts
+++ server/src/api/v1/models/user.model.ts
@@ -1,22 +1,23 @@
 import mongoose from "mongoose";
 import IUser from "../interfaces/user.interface";
 
-const UserSchema= new mongoose.Schema({
-   email: {
-       type:String,
-       required: true,
-       unique: true
-   },
-   password:{
-       type:String,
-       require:true
-   },
-   roles: [
-       {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Role" 
-       }
-   ],
+const UserSchema= new mongoose.Schema({ 
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        require: true
+    },
+    roles: [
+        {
+            type: String,
+            default: "mentor",
+            enum: ["mentor", "youth_mentor", "into_school_mentor", "women_mentor", "admin", "moderator"]
+        }
+    ],
 });
 
 const User = mongoose.model<IUser>(
