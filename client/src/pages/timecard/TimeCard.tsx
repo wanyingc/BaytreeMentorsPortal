a620352bb2f32690ab836a33378da4d3a661b2d3
eha45 <eha45@sfu.ca>
2021-11-06 22:15:37 -0700

    Added listing of session history with dummy values

--- client/src/pages/timecard/TimeCard.tsx
+++ client/src/pages/timecard/TimeCard.tsx
@@ -1,73 +1,149 @@
+import React, { useState } from 'react'
 import './timecard.css'
-import AnalogueClock from 'react-analogue-clock';
 import DateTime from './DateTime';
+import EventCalendar from '../../components/EventCalendar';
+import { Container, Col, Row, ListGroup, Form } from "react-bootstrap/";
+import BootstrapTable from 'react-bootstrap-table-next';
 
-// Analog clock imported from https://www.npmjs.com/package/react-analogue-clock
+// Event Calendar imported from https://www.npmjs.com/package/react-big-calendar
 
-const clockOptions = {
-  baseColor: '#22345E',
-  borderColor: '#000000',
-  borderWidth: 0,
-  centerColor: '#DA2E7C',
-  handColors: {
-      hour: '#FFFFFF',
-      minute: '#FFFFFF',
-      second: '#DA2E7C',
+
+const mentees = [
+  {
+      id: 1,
+      name: 'Mentee 1',
   },
-  notchColor: '#51638D',
-  numbersColor: '#51638D',
-  showNumbers: false,
-  size: 300
-}
+  {
+      id: 2,
+      name: 'Mentee 2',
+  },
+  {
+      id: 3,
+      name: 'Mentee 3',
+  },
+];
+
+const sessionHistory = [
+  {
+    id: 1,
+    mentee: 'Mentee 1',
+    date: '2021-11-06',
+    start: '3:00PM',
+    end: '4:00PM',
+    notes: 'Worked hard on math'
+  },
+  {
+    id: 2,
+    mentee: 'Mentee 2',
+    date: '2021-11-05',
+    start: '2:00PM',
+    end: '3:00PM',
+    notes: 'Worked hard on english'
+  },
+  {
+    id: 3,
+    mentee: 'Mentee 3',
+    date: '2021-11-04',
+    start: '1:00PM',
+    end: '2:00PM',
+    notes: 'Worked hard on cooking'
+  },  
+];
 
 
 function TimeCard(): JSX.Element {
+
+  // const renderSessionHistory = sessionHistory.map((mentorType, index) => {
+  //   return(
+  //       <ListGroup.Item 
+  //           key={index} 
+  //       >
+  //           {mentorType}
+  //       </ListGroup.Item>
+  //   );
+  // });
+
+
+  const dateValue: Date = new Date();
   return (
-    <div id="container" className="container-lg">
-      <div className="col-xl-6">
-        <div className="row justify-content-center">
-            <button type="button" className="btn btn-default light-red-background w-50 rounded-pill">
-              Upload Notes
-            </button>
-        </div>
+    <Container>
+      <h3>Create Session</h3>
+      <hr />
 
-        <div className="row justify-content-center">
-            <button type="button" className="btn btn-default dark-red-background w-50 rounded-pill my-3">
-              Previous Time Punches
-            </button>
-        </div>
+      <Row>
 
-        <div className="row justify-content-center">
-          <div id="analogClock">
-            <AnalogueClock {...clockOptions}/>
+        <Col sm={5} md={5} lg={5}>
+        
+            <Row as={Row} className="mb-3">
+              <Col md={4}>
+                <label>Mentee:</label>
+              </Col>
+              <Col md={8}>
+                <select id="selectMentee" className="form-select" aria-label="Default select example">
+                  {mentees.map(mentee => (
+                    <option key={mentee.id} value={mentee.name}> {mentee.name} </option>
+                  ))}
+                </select>
+              </Col>
+            </Row>
+            
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="4">
+                Date:
+              </Form.Label>
+              <Col sm="8"> 
+                <Form.Control type="date" />
+              </Col>
+            </Form.Group>
+            
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="4">
+              Start Time:
+              </Form.Label>
+              <Col sm="8"> 
+                <Form.Control type="time" />
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="4">
+                End Time:
+              </Form.Label>
+              <Col sm="8"> 
+                <Form.Control type="time" />
+              </Col>
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>
+                Notes:
+              </Form.Label>
+              <Form.Control as="textarea" rows={5} />
+            </Form.Group>
+
+        </Col>
+
+        <Col sm={7} md={7} lg={7}>
+        <h1 className="display-5 text-center">Session History</h1>
+
+          {/* Map some stuff out*/}
+          <div className="mb-3">
+          <ListGroup>
+            
+            {sessionHistory.map(session => (
+                    <ListGroup.Item key={session.id}> 
+                      <p className="lead">{session.mentee}</p>
+                      {session.date}<br/>
+                      {session.start}-{session.end}
+                      <p><strong>{session.notes}</strong></p>
+                    </ListGroup.Item>
+              ))}
+          </ListGroup>
           </div>
-        </div>
+        </Col>
 
-        <div className="row justify-content-center">
-          <div id="dateTimeDisplay">
-            <DateTime></DateTime>
-          </div>
-        </div>
-
-        <div className="row justify-content-center">
-          <button type="button" className="btn btn-primary  w-50 rounded-pill" 
-            onClick={() => {
-              console.log(new Date());
-            }}>
-            Clock In</button>
-        </div>
-
-        <div className="row justify-content-center">
-          <button type="button" className="btn btn-secondary w-50 rounded-pill mb-5 my-3" 
-            onClick={() => {
-              console.log(new Date())
-            }}>
-            Clock Out
-          </button>
-        </div>
-
-      </div>
-    </div>
+      </Row>
+    </Container>
   );
 }
 
