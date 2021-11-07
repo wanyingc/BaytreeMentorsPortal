e4b5d2df663efe84f820dfa875c4667aef77106f
shasib <saqib_hasib@sfu.ca>
2021-11-07 08:05:05 -0800

    QuickFix: Add "add mentor" button to sidenav, remove "search" button

--- client/src/components/SidenavItems.tsx
+++ client/src/components/SidenavItems.tsx
@@ -49,10 +49,6 @@ export const volunteerItems2 = [
 ];
 export const adminItems1 = [
     {
-        to: "/search",
-        icon: <BsIcons.BsSearch className="nav-icon"/>
-    },
-    {
         to: "/dashboard",
         icon: <BsIcons.BsHouseDoor className="nav-icon"/>
     },
@@ -67,6 +63,10 @@ export const adminItems1 = [
     {
         to:"/mentors-list",
         icon: <BsIcons.BsPeople className="nav-icon"/>
+    },
+    {
+        to: "/signup",
+        icon: <BsIcons.BsPersonPlus className="nav-icon"/>
     },
 ];
 export const adminItems2 = [
