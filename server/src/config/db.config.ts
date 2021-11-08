b379a4ebc15d6f31a7370e07a442e628808d534d
Dhairya Kalra <dhairyakalra@icloud.com>
2021-11-07 23:02:12 -0800

    Changes back to localhost

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -1,3 +1,7 @@
-const DBConfig ="mongodb+srv://group_Mars:tothemoon@cluster0.bby83.mongodb.net/test?authSource=admin&replicaSet=atlas-1zcv1d-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
+const DBConfig = {
+    HOST: "localhost",
+    PORT: "27017",
+    DB: "BaytreeTeamMarsDatabase"
+};
 
 export default DBConfig;
