53b7ae4b4b217ab4be0129f7d1f05fc25d2fac51
eha45 <eha45@sfu.ca>
2021-11-19 03:01:54 -0800

    Set default profile picture as pink anonymous human

--- client/src/pages/profile/Profile.tsx
+++ client/src/pages/profile/Profile.tsx
@@ -13,8 +13,12 @@ import Axios from "axios";
 import'../records/Records.css';
 import { BASE_API_URL } from "../../config/config";
 
+// Img Source: https://pixabay.com/illustrations/profile-profile-pic-human-face-2398783/
+import defaultProfilePic from './default_profile_picture.webp';
+
 const DEFAULT_USER = {
-  profileImg: 'https://merodesk.com/wp-content/uploads/2021/05/user-4.png'
+  // profileImg: 'https://merodesk.com/wp-content/uploads/2021/05/user-4.png'
+  profileImg: defaultProfilePic
 };
 
 const ReduxFormSelect: any = (field: any) => (
