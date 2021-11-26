da8333bc4e6a604311e11bbc05ba56e6361941b5
wanyingc <wan_ying_chan@sfu.ca>
2021-11-26 06:45:34 -0800

    Change design of the page.

--- client/src/pages/dashboard/DashboardVolunteer.tsx
+++ client/src/pages/dashboard/DashboardVolunteer.tsx
@@ -1,9 +1,6 @@
 import { MyMentees, goalsList, notificationsList, doughnutChartData, barChartData } from './DashboardDataVolunteer'
-import { columnsMyMentees } from './DashboardDatatypes';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-import BootstrapTable from 'react-bootstrap-table-next';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
-import paginationFactory from 'react-bootstrap-table2-paginator';
 import { ListGroup }from 'react-bootstrap/';
 import DashboardDoughnut from '../../components/DashboardComponents/DashboardDoughnut';
 import { DashboardBarChart } from '../../components/DashboardComponents/DashboardBarChart';
@@ -26,7 +23,7 @@ function DashboardVolunteer() {
     return (
         <div className="container p-2 mt-5">
             <div className="row" id='dashboard-title'>
-                <h5 style={{fontSize: 65, color:'#FF1E89'}}>Welcome, <strong>User</strong>!</h5>
+                <h5 style={{fontSize: 65, color:'#FF1E89'}}>Welcome, <strong>Mentor</strong>!</h5>
             </div>
 
             <div className="row justify-content-center">
@@ -51,7 +48,7 @@ function DashboardVolunteer() {
                     <h2 className="dashboard-title">Active Goals</h2>
                     <ListGroup data-spy="scroll">
                         {goalsList.map(goals => (
-                            <ListGroup.Item key={goals.id}>
+                            <ListGroup.Item className="list-group" key={goals.id}>
                                 <div className="ms-2 me-auto">
                                     <div className="fw-bold">{goals.mentee}, {goals.date}</div>
                                     <div className="reviewDate">Review on {goals.reviewDate}</div>
@@ -67,7 +64,7 @@ function DashboardVolunteer() {
                     <h2 className="dashboard-title"> Active Mentees</h2>
                     <ListGroup data-spy="scroll">
                         {MyMentees.map(mentee => (
-                            <ListGroup.Item key={mentee.name}>
+                            <ListGroup.Item className="list-group" key={mentee.name}>
                                 <div className="ms-2 me-auto">
                                     <div className="item-listgroup">{mentee.name}</div>
                                     <div className="listgroup-info">D.O.B.: {mentee.dateOfBirth}</div>
@@ -84,14 +81,14 @@ function DashboardVolunteer() {
                 <div className="col-lg-4 col-md-4 mb-4">
                     <h2 className="dashboard-title">Latest Notifications</h2>
                     <ListGroup data-spy="scroll">
-                            {notificationsList.map(notifs => (
-                                <ListGroup.Item key={notifs.title}>
-                                    <div className="item-listgroup">{notifs.title}</div>
-                                    <div className="listgroup-info">{notifs.date}, {notifs.time}</div>
-                                    <div className="listgroup-info">{notifs.message}</div>
-                                </ListGroup.Item>
-                            ))}
-                        </ListGroup>
+                        {notificationsList.map(notifs => (
+                            <ListGroup.Item className="list-group" key={notifs.title}>
+                                <div className="item-listgroup">{notifs.title}</div>
+                                <div className="listgroup-info">{notifs.date}, {notifs.time}</div>
+                                <div className="listgroup-info">{notifs.message}</div>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
                 </div>
 
             </div>
