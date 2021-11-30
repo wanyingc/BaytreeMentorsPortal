3a9abf6f9d9fcf82560c8caec7d38c13a0b61137
wanyingc <wan_ying_chan@sfu.ca>
2021-11-29 17:46:02 -0800

    Create list of goals to display.

---
+++ client/src/interfaces/GoalsInterface.tsx
@@ -0,0 +1,6 @@
+export type goalsObject = {
+    id: number;
+    mentee: string;
+    date: string;
+    notes: string;
+}
--- client/src/pages/goals/Goals.tsx
+++ client/src/pages/goals/Goals.tsx
@@ -1,12 +1,29 @@
-
-
+import './goals.css';
+import { Form, Row, Col, ListGroup, Button } from 'react-bootstrap/';
+import { goalsList } from './GoalsData';
 
 const Goals = () => {
 
-
     return (
       <div className="container">
-    
+        <div className="row">
+          <h2 className="page-title">Goals</h2>
+        </div>
+        
+        <div className="row">
+          <div className="col-lg-6 col-md-6 mb-4">
+            <ListGroup data-spy="scroll">
+              {goalsList.map(goals => (
+                <ListGroup.Item key={goals.id}>
+                  <p className="lead"><strong>{goals.mentee}, {goals.date}</strong></p>
+                  <p>{goals.notes}</p>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+
+          </div>
+
+        </div>
       </div>
     );
 }
