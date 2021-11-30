5e1ec0f762d696289e63c9f8bcf84e630f70e27a
Dhairya Kalra <dhairyakalra@icloud.com>
2021-11-30 01:18:25 -0800

    Changed the layout of forgot password and reset password

--- client/src/pages/forgot-password/ForgotPassword.tsx
+++ client/src/pages/forgot-password/ForgotPassword.tsx
@@ -6,7 +6,7 @@ function Forgot() {
     <div className="container-fluid">
       <div className="row">
       <div className="login-image align-items-center">
-        <div className=" col-md-6 offset-md-3 align-self-center d-flex justify-content-center background-color-light p-3 mb-2 bg-white rounded mt-4 opacity- 15">
+        <div className=" col-md-4 offset-md-3 align-self-center d-flex justify-content-center background-color-light p-3 mb-3 bg-white rounded mt-5 opacity- 15">
           <ForgotComponent/>
         </div>
       </div>
