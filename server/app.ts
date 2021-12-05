0aeeb99b2d3f6655201687c254ae126b8f9da3eb
eha45 <eha45@sfu.ca>
2021-12-05 10:49:42 -0800

    Created controller and router for session notes

--- server/app.ts
+++ server/app.ts
@@ -18,6 +18,7 @@ import forgotRoute from './src/api/v1/routes/forgotPass.route'
 import resetRoute from './src/api/v1/routes/resetPass.route'
 import mentorHomeRouter from './src/api/v1/routes/mentorhome.route';
 import questionsListRouter from './src/api/v1/routes/questionslist.route'
+import sessionRouter from './src/api/v1/routes/session.route';
 
 const app=express();
 
@@ -43,6 +44,7 @@ app.use("/api", forgotRoute);
 app.use("/api", resetRoute);
 app.use("/api", mentorHomeRouter);
 app.use("/api", questionsListRouter);
+app.use("/api", sessionRouter);
 
 // This router is only for testing, and demonstrates example for authority based API requests
 app.use("/api", testRouter);
---
+++ server/src/api/v1/routes/session.route.ts
@@ -0,0 +1,25 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import sessionController from '../controllers/session.controller';
+import authMW from '../middlewares/auth.middleware';
+
+const sessionRouter = Router();
+
+sessionRouter.use((req:Request, res:Response, next:NextFunction) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header(
+        "Access-Control-Allow-Headers", 
+        "x-access-token, Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+    );
+    next();
+});
+
+sessionRouter.post(
+    `/auth/session-notes/`, 
+    [
+        authMW.verifyJWT,
+        authMW.isUser
+    ],  
+    sessionController);
+
+export default sessionRouter;
