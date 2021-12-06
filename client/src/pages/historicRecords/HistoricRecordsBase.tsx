8cb2a39193ae495527a0b483fbdfe244f9349bbc
eha45 <eha45@sfu.ca>
2021-12-06 03:18:22 -0800

    Added text to tell mentors to talk to admins if they want to edit previous sessions

--- client/src/pages/historicRecords/HistoricRecordsBase.tsx
+++ client/src/pages/historicRecords/HistoricRecordsBase.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect  } from 'react';
 import Axios from 'axios';
 import'./HistoricRecords.css';
-import { Col, Container, Row, Spinner, Table } from 'react-bootstrap';
+import { Form, Col, Container, Row, Spinner, Table } from 'react-bootstrap';
 import BootstrapTable from 'react-bootstrap-table-next';
 import { getAccessToken } from "../../auth/Authenticator";
 import { BASE_API_URL } from '../../config/config';
@@ -49,78 +49,81 @@ const RecordsComponent = (props:any) => {
             </div>
           }  
 
-            <h2>Historic Records</h2>
-            <hr />
+          <h2>Historic Records</h2>
+          <hr />
 
-            <div>
-              <div className="d-flex justify-content-between align-items-center mb-3">
-                <h4 className="text-center">{props.sessionTitle}</h4>
-              </div>
-                <Table striped bordered hover >
-                    <thead>
-                      <tr>
-                        <th>Session ID</th>
-                        <th>Date / Duration</th>
-                        <th>Title</th>
-                        <th>Type</th>
-                        <th>Status</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      { 
-                        mentorRecords?.sessions.map((sessionInfo: any) => {
-                          return (
-                            <tr key={sessionInfo["SessionID"]}
-                                onClick={() => {
-                                  history.push(`/session/` + props.personID + `/` + sessionInfo["SessionID"]); 
-                                }}>
-                              <td>{sessionInfo["SessionID"]}</td>
-                              <td>{sessionInfo["StartDate"]} / {sessionInfo["Duration"]}</td>
-                              <td>{sessionInfo["Title"]}</td>
-                              <td>{sessionInfo["Type"]}</td>
-                              <td>{sessionInfo["Status"]}</td>
-                            </tr>
-                          );
-                        })
-                      }
-                    </tbody>
-                  </Table>
-
-
-
-                  <div className="d-flex justify-content-between align-items-center mb-3">
-                    <h4 className="text-center">{props.questionnaireTitle}</h4>
-                  </div>
-
-                  <Table striped bordered hover>
-                    <thead>
-                      <tr>
-                        <th>Questionnaire ID</th>
-                        <th>Date</th>
-                        <th>Name</th>
-                        <th>Type</th>
-                        <th>Questionnaire</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      { 
-                        mentorRecords?.questionnaires.map((qInfo: any) => {
-                          return (
-                            <tr key={qInfo["QuestionnaireID"]}>
-                              <td>{qInfo["QuestionnaireID"]}</td>
-                              <td>{qInfo["Date"]}</td>
-                              <td>{qInfo["EntityNiceName"]}</td>
-                              <td>{qInfo["EntityType"]}</td>
-                              <td>{qInfo["Questionnaire"]}</td>
-                              
-                            </tr>
-                          )
-                        })
-                      }
-                    </tbody>
-                  </Table>
+          <div>
+            <div className="d-flex justify-content-between align-items-center mb-3">
+              <h4 className="text-center">{props.sessionTitle}</h4>
             </div>
-      </Col>
+            <Table striped bordered hover >
+              <thead>
+                <tr>
+                  <th>Session ID</th>
+                  <th>Date / Duration</th>
+                  <th>Title</th>
+                  <th>Type</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                { 
+                  mentorRecords?.sessions.map((sessionInfo: any) => {
+                    return (
+                      <tr key={sessionInfo["SessionID"]}
+                        onClick={() => {
+                            history.push(`/session/` + props.personID + `/` + sessionInfo["SessionID"]); 
+                        }}>
+                        <td>{sessionInfo["SessionID"]}</td>
+                        <td>{sessionInfo["StartDate"]} / {sessionInfo["Duration"]}</td>
+                        <td>{sessionInfo["Title"]}</td>
+                        <td>{sessionInfo["Type"]}</td>
+                        <td>{sessionInfo["Status"]}</td>
+                      </tr>
+                    );
+                  })
+                }
+              </tbody>
+            </Table>
+            <div className="text font-weight-light">
+                * To edit sessions, please contact the administrator.
+            </div>
+          
+                
+
+
+            <div className="d-flex justify-content-between align-items-center mb-3">
+              <h4 className="text-center">{props.questionnaireTitle}</h4>
+            </div>
+
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Questionnaire ID</th>
+                  <th>Date</th>
+                  <th>Name</th>
+                  <th>Type</th>
+                  <th>Questionnaire</th>
+                </tr>
+              </thead>
+              <tbody>
+                { 
+                  mentorRecords?.questionnaires.map((qInfo: any) => {
+                    return (
+                      <tr key={qInfo["QuestionnaireID"]}>
+                        <td>{qInfo["QuestionnaireID"]}</td>
+                        <td>{qInfo["Date"]}</td>
+                        <td>{qInfo["EntityNiceName"]}</td>
+                        <td>{qInfo["EntityType"]}</td>
+                        <td>{qInfo["Questionnaire"]}</td>                          
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </Table>
+          </div>
+        </Col>
       </Row>   
     </Container>  
   );
